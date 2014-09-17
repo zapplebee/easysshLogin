9e7b31358cd3de7a4d4833872e7da3722b1623a6
Zachary Skalko <zskalko@gmail.com>
2014-09-17 14:41:30 -0500

    Add return false for failed connections

--- index.php
+++ index.php
@@ -6,15 +6,13 @@ set_include_path("phpseclib/phpseclib");
 include("Net/SFTP.php");
 
 /*
-  Set your server details. In this case it has been included in "../../credentials.php".;
+  Set your server details & test cases. In this case it has been included in "../../credentials.php".;
 
   $sshServer = array(
     "address" => "YOUR SERVER ADDRESS",
     "port"    => "YOUR SERVER PORT",
     "secret"  => "YOUR SECRET ENCRYPTION PHRASE",
   );
-
-  Set test cases. In this case it has been included in "../../credentials.php".
   
   $testCases = array("user" => "pass");
 */
@@ -24,7 +22,9 @@ class user{
   private $address;
   private $port;
   private $secret;
-  private $user;
+  public $user;
+  public $errors = array();
+  private $connectionStatus = false;
 
   function __construct($creds){
   
@@ -44,43 +44,84 @@ class user{
     $pattern ="#[^A-Za-z0-9\._-]#";
     preg_match($pattern, $user, $results);
     if(!empty($results) || $user[0] == "-" || strlen($user) > 8 || strlen($user) == 0){
-    
-      return "Bad Username";
+      $this->errors[] = "Bad Username.";
+      return false;
       
     }
 
     $pattern ="#[\n\t\r]#";
     preg_match($pattern, $pass, $results);
     if(!empty($results)){
-    
-      return "Bad Password";
+      $this->errors[] = "Bad Password.";
+      return false;
       
     }
   
     $SSH = new Net_SSH2($this->address, $this->port);
     if (!$SSH->login($user, $pass)) {
-    
-      return ("Connection to server failed.");
+      $this->errors[] = "Bad connection or incorrect credentials.";
+      return false;
       
     }
     else {
     
       $this->user = $user;
+      $this->connectionStatus = true;
       $_SESSION['easysshLogin'][$this->user] = openssl_encrypt(json_encode(array($user,$pass)), "bf-ecb", $this->secret);
-      return "Good connection and credentials";
+      return true;
       
     }
 
   }
   
-  public function getUserCreds(){
+  private function getUserCreds(){
   
     if (!isset($_SESSION['easysshLogin'][$this->user])){
-      return "Did not log in";
+      return false;
     }
     $creds = json_decode(openssl_decrypt($_SESSION['easysshLogin'][$this->user], "bf-ecb", $this->secret),true);
     return $creds;
   
+  }
+  
+  
+  
+  public function sshConnection(){
+    
+    $creds = $this->getUserCreds();
+  
+    
+    if (!$this->connectionStatus){
+      return false;
+    }
+    
+    $SSH = new Net_SSH2($this->address, $this->port);
+    if (!$SSH->login($creds[0], $creds[1])) {
+      $this->errors[] = "Bad connection or incorrect credentials.";
+      return false;
+    }
+    
+    return $SSH;
+
+  }
+  
+  public function sftpConnection(){
+    
+    $creds = $this->getUserCreds();
+  
+    
+    if (!$this->connectionStatus){
+      return false;
+    }
+    
+    $SFTP = new Net_SFTP($this->address, $this->port);
+    if (!$SFTP->login($creds[0], $creds[1])) {
+      $this->errors[] = "Bad connection or incorrect credentials.";
+      return false;
+    }
+    
+    return $SFTP;
+
   }
 
 }
@@ -91,11 +132,9 @@ $user = new user($sshServer);
 
 foreach ($testCases as $key => $pass){
 
-  echo "<h3>$key</h3>";
-  echo "<h3>$pass</h3>";
-  echo $user->login($key,$pass);
-  echo "<HR>";
-  print_r ($user->getUserCreds());
+  $user->login($key,$pass);
+  $conn = $user->sshConnection();
+  echo $conn->exec("date\n");
   echo "<HR>";
 
 }
