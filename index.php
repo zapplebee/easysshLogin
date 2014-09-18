e09246044799182f9a7297c60156962d92b04844
Zachary Skalko <zskalko@gmail.com>
2014-09-17 22:50:07 -0500

    Add form to test class to index

--- index.php
+++ index.php
@@ -5,52 +5,46 @@
 </head>
 <body>
 <h1>easysshLogin Demo</h1>
-<h3>Set your server details & test cases.</h3>
+<h3>Set your server and user details.</h3>
+<form action="" method="POST">
 <pre>
   $sshServer = array(
-    "address" => "YOUR SERVER ADDRESS",
-    "port"    => "YOUR SERVER PORT",
-    "secret"  => "YOUR SECRET ENCRYPTION PHRASE",
+    "address" => <input type="text" name="address" value="<?php if(isset($_POST['address'])){echo $_POST['address'];}?>" placeholder="Your Server Address">, <span style="color:red"></span>
+    "port"    => <input type="text" name="port" value="<?php if(isset($_POST['port'])){echo $_POST['port'];}?>" placeholder="Your Server Port">,
+    "secret"  => <input type="password" name="secret" value="<?php if(isset($_POST['secret'])){echo $_POST['secret'];}?>" placeholder="An encryption key">,
   );
   
-  $testCase = array("user","pass");
+  $credentials = array(
+    "user"    => <input type="text" name="user" value="<?php if(isset($_POST['user'])){echo $_POST['user'];}?>" placeholder="Your Username">, <span style="color:red">// Must fit the regex [^A-Za-z0-9\._-], cannot start with "-", cannot be longer than 8 characters.</span>
+    "pass"    => <input type="password" name="pass" value="<?php if(isset($_POST['pass'])){echo $_POST['pass'];}?>" placeholder="Your Password">, <span style="color:red">// Must fit the regex [\n\t\r]</span>
+    
+   );
 </pre>
-<pre>
+<input type="submit" value="Submit">
+</form>
+
 <?php
-include("../../credentials.php");
 include('classes.php');
-/*
-  Set your server details & test cases. In this case it has been included in "../../credentials.php".;
 
-  $sshServer = array(
-    "address" => "YOUR SERVER ADDRESS",
-    "port"    => "YOUR SERVER PORT",
-    "secret"  => "YOUR SECRET ENCRYPTION PHRASE",
-  );
-  
-  $testCase = array("user","pass");
-*/
+ if(!isset($_POST['address']) || !isset($_POST['port']) || !isset($_POST['secret']) || !isset($_POST['user']) || !isset($_POST['pass'])){
+ echo "</body></html>";
+ exit;
+ }
+
+  $user = new easysshLogin(array("address" => $_POST['address'],"port" => $_POST['port'], "secret" => $_POST['secret']));
 
 
-  $user = new easysshLogin($sshServer);
-
- // $user->login($testCase[0],$testCase[1]);
- // $conn = $user->sshConnection();
- // echo $conn->exec("date");
- // echo "\n";
-  
 
 ?>
-</pre>
 <h3>The easysshLogin object public attributes and functions</h3>
 
 <h4>$user->login($user,$pass)</h4>
 <p>Returns a boolean value indicating the login success or failure. This must be executed first.</p> 
 <pre>
-echo $user->login($testCase[0],$testCase[1]);
+echo $user->login("username","password");
 
 <b>Output:</b>
-<?php echo $user->login($testCase[0],$testCase[1]);?>
+<?php echo $user->login($_POST['user'],$_POST['pass']);?>
 </pre>
 <hr>
 
